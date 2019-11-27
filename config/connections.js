0c2b328057bebadc1f132ef03ecbf7788533f00a
Chase Laptop <james.chase.richards@gmail.com>
2019-11-26 21:41:54 -0500

    cleans up config

--- config/connections.js
+++ config/connections.js
@@ -7,19 +7,12 @@ if (process.env.JAWSDB_URL) {
 } else {
   connection = mysql.createConnection({
     host: "localhost",
+    port: 3306,
     user: "root",
     password: "docker",
     database: "burger"
   });
 }
-
-// connection = mysql.createConnection({
-//   host: "localhost",
-//   port: 3306,
-//   user: "root",
-//   password: "docker",
-//   database: "burger"
-// });
 
 // Make connection.
 connection.connect(function(err) {
