9c52ae076d22155d969eed0886dd3746b9bea2fb
Chase Laptop <james.chase.richards@gmail.com>
2019-11-26 20:37:50 -0500

    heroku setup

--- config/connections.js
+++ config/connections.js
@@ -1,5 +1,17 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "docker",
+    database: "burger"
+  });
+}
 
 var connection = mysql.createConnection({
   host: "localhost",
